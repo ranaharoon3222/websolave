1fc6b2c0491ba0eff0fe978ae87c6e21d5e79e82
Abdulrehman Akhter <hafizabdulrahman91@gmail.com>
2024-02-20 14:22:38 +0500

    Edit featureOne and ServiceBox slice

--- slices/FeatureOneSection/index.js
+++ slices/FeatureOneSection/index.js
@@ -11,9 +11,11 @@ const FeatureOneSection = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="container">
-        <div className="row align-items-center">
-          <FeatureOne slice={slice} />
+      <div className="fancy-feature-one pt-170 lg-pt-140">
+        <div className="container">
+          <div className="row align-items-center">
+            <FeatureOne slice={slice} />
+          </div>
         </div>
       </div>
     </section>
--- slices/ServiceBox/index.js
+++ slices/ServiceBox/index.js
@@ -11,21 +11,23 @@ const ServiceBox = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="container">
-        <div className="inner-content pt-110 lg-pt-60 md-pt-30">
-          <div className="row gx-xxl-5">
-            {slice.items.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`col-lg-3 col-sm-6`}
-                  data-aos="fade-up"
-                  data-aos-delay={`${index * 100}`}
-                >
-                  <Services item={item} />
-                </div>
-              );
-            })}
+      <div className="fancy-feature-one">
+        <div className="container">
+          <div className="inner-content pt-110 lg-pt-60 md-pt-30">
+            <div className="row gx-xxl-5">
+              {slice.items.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={`col-lg-3 col-sm-6`}
+                    data-aos="fade-up"
+                    data-aos-delay={`${index * 100}`}
+                  >
+                    <Services item={item} />
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
