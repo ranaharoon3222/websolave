fc4462864349627f1847e8397fe8279d27fbaf35
ranaharoon3222 <ranaharoon3222@gmail.com>
2024-02-26 11:54:37 +0500

    fixed

--- pages/404.js
+++ pages/404.js
@@ -1,35 +1,34 @@
-import Link from "next/link";
-import Seo from "../components/common/Seo";
-import DefaulHeader from "../components/header/DefaulHeader";
-import Image from "next/image";
+import Link from 'next/link';
+import Seo from '../components/common/Seo';
+import DefaulHeader from '../components/header/DefaulHeader';
+import Image from 'next/image';
 
 const Pricing = () => {
   return (
     <>
-      <Seo pageTitle="404" />
+      <Seo pageTitle='404' />
 
       {/* <!-- 
       =============================================
       Theme Default Menu
       ============================================== 	
       --> */}
-      <DefaulHeader />
 
       {/* 
 			=============================================
 			Error Page
 			============================================== 
 			*/}
-      <div className="error-page-content d-flex align-items-center justify-content-center">
-        <div className="container">
-          <div className="row">
-            <div className="col-xxl-6 col-lg-7 m-auto">
+      <div className='error-page-content d-flex align-items-center justify-content-center'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-xxl-6 col-lg-7 m-auto'>
               <h3>Opps! you’r on the wrong place.</h3>
-              <p className="me-xxl-5 ms-xxl-5 pt-15 pb-20">
+              <p className='me-xxl-5 ms-xxl-5 pt-15 pb-20'>
                 Can not find what you need? Take a moment and do a search below
                 or start from our Homepage.
               </p>
-              <Link href="/" className="btn-twentyOne fw-500 tran3s">
+              <Link href='/' className='btn-twentyOne fw-500 tran3s'>
                 Back to home
               </Link>
             </div>
@@ -37,9 +36,9 @@ const Pricing = () => {
           <Image
             width={800}
             height={522}
-            src="/images/assets/ils_06.svg"
-            alt=""
-            className="m-auto"
+            src='/images/assets/ils_06.svg'
+            alt=''
+            className='m-auto'
           />
         </div>
         {/* End .container */}
@@ -47,9 +46,9 @@ const Pricing = () => {
         <Image
           width={1915}
           height={674}
-          src="/images/shape/shape_178.svg"
-          alt="shape"
-          className="shapes shape-one w-100"
+          src='/images/shape/shape_178.svg'
+          alt='shape'
+          className='shapes shape-one w-100'
         />
       </div>
       {/* /.error-page-content */}
--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,15 +1,15 @@
-import Seo from "../../components/common/Seo";
-import DefaulHeader from "../../components/header/DefaulHeader";
-import DefaultFooter from "../../components/footer/DefaultFooter";
-import SocialShare from "../../components/portfolio/portfolio-details/SocialShare";
-import ProjectDetails from "../../components/portfolio/portfolio-details/ProjectDetails";
-import ProjectSlide from "../../components/portfolio/portfolio-details/ProjectSlide";
-import CallToAction from "../../components/portfolio/CallToAction";
-import PortfolioGallery from "../../components/portfolio/portfolio-details/PortfolioGallery";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import portfolioData from "../../data/portfolio";
-import PortfolioDetailsTitle from "../../components/portfolio/portfolio-details/PortfolioDetailsTitle";
+import Seo from '../../components/common/Seo';
+import DefaulHeader from '../../components/header/DefaulHeader';
+import DefaultFooter from '../../components/footer/DefaultFooter';
+import SocialShare from '../../components/portfolio/portfolio-details/SocialShare';
+import ProjectDetails from '../../components/portfolio/portfolio-details/ProjectDetails';
+import ProjectSlide from '../../components/portfolio/portfolio-details/ProjectSlide';
+import CallToAction from '../../components/portfolio/CallToAction';
+import PortfolioGallery from '../../components/portfolio/portfolio-details/PortfolioGallery';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import portfolioData from '../../data/portfolio';
+import PortfolioDetailsTitle from '../../components/portfolio/portfolio-details/PortfolioDetailsTitle';
 
 const DynamicPortfolioDetails = () => {
   const router = useRouter();
@@ -25,13 +25,13 @@ const DynamicPortfolioDetails = () => {
 
   return (
     <>
-      <Seo pageTitle="Portfolio Details" />
+      <Seo pageTitle='Portfolio Details' />
       {/* <!-- 
       =============================================
       Theme Default Menu
       ============================================== 	
       --> */}
-      <DefaulHeader />
+      {/* <DefaulHeader /> */}
 
       {/* 
         =============================================
@@ -45,23 +45,23 @@ const DynamicPortfolioDetails = () => {
 				Portfolio Details Two
 			============================================== 
 			*/}
-      <div className="portfolio-details-two pt-70 pb-50 lg-pb-10 md-pt-10">
-        <div className="project-desctiption">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-8" data-aos="fade-right">
+      <div className='portfolio-details-two pt-70 pb-50 lg-pb-10 md-pt-10'>
+        <div className='project-desctiption'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-lg-8' data-aos='fade-right'>
                 <ProjectSlide slide={portfolio} />
                 {/* /#gallery-carousel */}
               </div>
               {/* End .col-lg-8 */}
 
-              <div className="col-lg-4" data-aos="fade-left">
-                <div className="sidebar ms-xl-5">
-                  <h3 className="mb-20">About</h3>
-                  <p className="border-bottom pb-40 mb-35 lg-pb-20">
+              <div className='col-lg-4' data-aos='fade-left'>
+                <div className='sidebar ms-xl-5'>
+                  <h3 className='mb-20'>About</h3>
+                  <p className='border-bottom pb-40 mb-35 lg-pb-20'>
                     The Internet advertising famous today behaved lately.
                   </p>
-                  <div className="row">
+                  <div className='row'>
                     <ProjectDetails details={portfolio} />
                   </div>
                   {/* End .row */}
@@ -73,18 +73,18 @@ const DynamicPortfolioDetails = () => {
             </div>
             {/* End .row */}
 
-            <div className="col-xl-9  mt-120 lg-mt-80">
+            <div className='col-xl-9  mt-120 lg-mt-80'>
               <div
-                className="title-style-twelve mb-45 lg-mb-30"
-                data-aos="fade-up"
+                className='title-style-twelve mb-45 lg-mb-30'
+                data-aos='fade-up'
               >
-                <div className="sc-title fst-italic position-relative">
+                <div className='sc-title fst-italic position-relative'>
                   Overview
                 </div>
-                <h2 className="main-title fw-500 tx-dark">About Rodpen</h2>
+                <h2 className='main-title fw-500 tx-dark'>About Rodpen</h2>
               </div>
               {/* /.title-style-twelve */}
-              <p data-aos="fade-up">
+              <p data-aos='fade-up'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullaum laboris
@@ -92,32 +92,32 @@ const DynamicPortfolioDetails = () => {
                 in reprehenderit volupta velit esse cillum dolore eu fugiat
                 nulla pariatur.
               </p>
-              <p data-aos="fade-up">
+              <p data-aos='fade-up'>
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa
                 qui officia deserunt mollit anim id est laborum magna quis
                 nostured.
               </p>
-              <div className="row">
-                <div className="col-xl-8">
+              <div className='row'>
+                <div className='col-xl-8'>
                   <div
-                    className="title-style-twelve mb-45 pt-75 lg-pt-40 lg-mb-30"
-                    data-aos="fade-up"
+                    className='title-style-twelve mb-45 pt-75 lg-pt-40 lg-mb-30'
+                    data-aos='fade-up'
                   >
-                    <div className="sc-title fst-italic position-relative">
+                    <div className='sc-title fst-italic position-relative'>
                       Work Proccess
                     </div>
-                    <h2 className="main-title fw-500 tx-dark">
+                    <h2 className='main-title fw-500 tx-dark'>
                       Find out the design solution.
                     </h2>
                   </div>
                   {/* /.title-style-twelve */}
                 </div>
               </div>
-              <p data-aos="fade-up">
+              <p data-aos='fade-up'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
-              <p data-aos="fade-up">
+              <p data-aos='fade-up'>
                 Enim eu turpis egestas pretium aenean pharetra. Dui accumsan sit
                 amet nulla facilisi morbi tempus iaculis. Eu ultrices vitae
                 auctor eu augue. Sed turpis tincidunt id aliquet risus Purus in
@@ -125,28 +125,28 @@ const DynamicPortfolioDetails = () => {
                 augue eget arcu dictum. Gravida quis blandit turpis cursus in
                 hac habitasse platea dictumst.
               </p>
-              <div className="row">
+              <div className='row'>
                 <PortfolioGallery />
               </div>
 
               <div
-                className="title-style-twelve mb-45 mt-120 lg-mb-30 lg-mt-80"
-                data-aos="fade-up"
+                className='title-style-twelve mb-45 mt-120 lg-mb-30 lg-mt-80'
+                data-aos='fade-up'
               >
-                <div className="sc-title fst-italic position-relative">
+                <div className='sc-title fst-italic position-relative'>
                   Work Proccess
                 </div>
-                <h2 className="main-title fw-500 tx-dark">Final Result</h2>
+                <h2 className='main-title fw-500 tx-dark'>Final Result</h2>
               </div>
               {/* /.title-style-twelve */}
 
-              <p data-aos="fade-up">
+              <p data-aos='fade-up'>
                 Content creators and human resources personnel are able to
                 seamlessly update the website through graphical interfaces, and
                 the site simply rebuilds itself along with search engine indexes
                 as the OpenWeb team continues to create.
               </p>
-              <p data-aos="fade-up">
+              <p data-aos='fade-up'>
                 Enim eu turpis egestas pretium aenean pharetra. Dui accumsan sit
                 amet nulla facilisi mor tempu iaculis. Eu ultrices vitae auctor
                 eu augue. Sed turpis tincidunt id aliquet risus Purus in massa
@@ -157,27 +157,27 @@ const DynamicPortfolioDetails = () => {
             </div>
             {/* End col-xl-9 */}
 
-            <div className="project-pagination m-auto pt-100 lg-pt-50 sm-pt-10">
-              <div className="row gx-xxl-5">
-                <div className="col-sm-6">
+            <div className='project-pagination m-auto pt-100 lg-pt-50 sm-pt-10'>
+              <div className='row gx-xxl-5'>
+                <div className='col-sm-6'>
                   <div
-                    className="arrow-block position-relative zn2 mt-20"
-                    data-aos="fade-right"
+                    className='arrow-block position-relative zn2 mt-20'
+                    data-aos='fade-right'
                   >
                     <img
-                      src="/images/media/img_92.jpg"
-                      alt="media"
-                      className="lazy-img w-100"
+                      src='/images/media/img_92.jpg'
+                      alt='media'
+                      className='lazy-img w-100'
                     />
-                    <div className="hover-content tran3s position-absolute d-flex flex-column align-items-center justify-content-center">
-                      <div className="text-white fw-500 pg-title">
+                    <div className='hover-content tran3s position-absolute d-flex flex-column align-items-center justify-content-center'>
+                      <div className='text-white fw-500 pg-title'>
                         Prev Project
                       </div>
                       <a
-                        href="#"
-                        className="arrow rounded-circle text-center tran3s"
+                        href='#'
+                        className='arrow rounded-circle text-center tran3s'
                       >
-                        <i className="bi bi-arrow-left" />
+                        <i className='bi bi-arrow-left' />
                       </a>
                     </div>
                     {/* /.hover-content */}
@@ -186,25 +186,25 @@ const DynamicPortfolioDetails = () => {
                 </div>
                 {/* End .col-6 */}
 
-                <div className="col-sm-6">
+                <div className='col-sm-6'>
                   <div
-                    className="arrow-block position-relative zn2 mt-20"
-                    data-aos="fade-left"
+                    className='arrow-block position-relative zn2 mt-20'
+                    data-aos='fade-left'
                   >
                     <img
-                      src="/images/media/img_93.jpg"
-                      alt="media"
-                      className="lazy-img w-100"
+                      src='/images/media/img_93.jpg'
+                      alt='media'
+                      className='lazy-img w-100'
                     />
-                    <div className="hover-content tran3s position-absolute d-flex flex-column align-items-center justify-content-center">
-                      <div className="text-white fw-500 pg-title">
+                    <div className='hover-content tran3s position-absolute d-flex flex-column align-items-center justify-content-center'>
+                      <div className='text-white fw-500 pg-title'>
                         Next Project
                       </div>
                       <a
-                        href="#"
-                        className="arrow rounded-circle text-center tran3s"
+                        href='#'
+                        className='arrow rounded-circle text-center tran3s'
                       >
-                        <i className="bi bi-arrow-right" />
+                        <i className='bi bi-arrow-right' />
                       </a>
                     </div>
                     {/* /.hover-content */}
